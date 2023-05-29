592fda82926bbdc66c8d26a88f54adcba54be4f3
jacqueline-castro <jacquelinecastro.dev@gmail.com>
2023-05-28 22:16:14 -0300

    structuring main page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 function App() {
-  return <div></div>;
+  return (
+    <div className="flex h-screen justify-center items-center">
+      <div className="card w-[80%] lg:w-[50%] h-[70%] shadow-md rounded-sm transform ease-out duration-300 items-center p-10 gap-5">
+        <h1 className="text-5xl font-work font-bold w-fit text-center">
+          Quests To Do
+        </h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
