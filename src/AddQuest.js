6fd9cd99e556500eea1d272159c8cf40d8fed86f
jacqueline-castro <jacquelinecastro.dev@gmail.com>
2023-05-30 18:34:10 -0300

    sitting state in onChange

--- src/AddQuest.js
+++ src/AddQuest.js
@@ -7,6 +7,7 @@ function AddQuest(props) {
       <input
         placeholder="quest"
         className="rounded-full bg-secundary pl-2 input-sm flex w-[70%] focus:outline-none"
+        onChange={(e) => setTitle(e.target.value)}
       />
       <button
         className="flex items-center text-center rounded-full bg-primary h-fit px-2 text-lg transform ease-out duration-300"
