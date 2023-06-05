676eb822e43dae5edb59118745c70a87463b3687
jacqueline-castro <jacquelinecastro.dev@gmail.com>
2023-06-04 21:52:37 -0300

    basic component structure

--- src/QuestItem.js
+++ src/QuestItem.js
@@ -10,5 +10,22 @@ export default function QuestItem(props) {
   // define o visual da missão na lista
   const conclued = props.quest.status === "concluído";
 
-  return <div></div>;
+  return (
+    <div className="flex gap-4 flex-col md:flex-row items-center">
+      <div className="flex gap-4 items-center w-full sm:w-[80%]">
+        <input
+          disabled={conclued}
+          type="checkbox"
+          checked={checked}
+          className="checkbox rounded-full border"
+        />
+
+        <p className="break-words">{props.quest.title}</p>
+      </div>
+      <div className="flex gap-4 w-full sm:w-fit justify-center">
+        <button onClick={() => setEditMode(!editMode)}>Editar</button>
+        <button>Excluir</button>
+      </div>
+    </div>
+  );
 }
