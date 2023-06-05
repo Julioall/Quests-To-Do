5897ae11ac8ae22cf00467f98336a82220e2527d
jacqueline-castro <jacquelinecastro.dev@gmail.com>
2023-06-04 22:53:52 -0300

    passing functions from list to item

--- src/QuestList.js
+++ src/QuestList.js
@@ -4,7 +4,13 @@ export default function QuestList(props) {
   return (
     <div className="flex flex-col overflow-y-auto scrollbar-thin gap-6 w-[80%]">
       {props.quests.map((quest) => {
-        return <QuestItem quest={quest} />;
+        return (
+          <QuestItem
+            quest={quest}
+            saveEditQuest={props.saveEditQuest}
+            saveConcluedQuest={props.saveConcluedQuest}
+          />
+        );
       })}
     </div>
   );
